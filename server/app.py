a3c2ff9bfe071143a99899786a9e425624ceb99f
mwangiryan12 <162281245+mwangiryan12@users.noreply.github.com>
2024-07-18 15:29:15 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -17,10 +17,35 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        response = make_response([plant.to_dict() for plant in plants],200)
+        return response
+    
+    def post(self):
+        new_plant = Plant(
+            name = request.get_json()["name"],
+            image = request.get_json()["image"],
+            price = request.get_json()["price"]
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        response = make_response(new_plant.to_dict(),204,{"Content-Type":"application/json"})
+        return response
+
+api.add_resource(Plants,"/plants")
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        if plant:
+            response = make_response(plant.to_dict(),200)
+            return response
+        else:
+            response = make_response({"message":"Plant not found"},404)
+            return response
+
+api.add_resource(PlantByID,"/plants/<int:id>")
         
 
 if __name__ == '__main__':
