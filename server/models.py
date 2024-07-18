1e45ee123de56ef2cce4c7999eb2fcab4cefaab7
mwangiryan12 <162281245+mwangiryan12@users.noreply.github.com>
2024-07-18 15:30:22 +0300

    Update models.py

--- server/models.py
+++ server/models.py
@@ -1,9 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+metadata = MetaData()
+db = SQLAlchemy(metadata=metadata)
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
